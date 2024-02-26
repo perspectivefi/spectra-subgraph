fbb0b9e9e0d641c3446cde3c923d948c239f4c17
matstyler <mat.franczuk@gmail.com>
2024-02-26 09:45:13 +0100

    chore: sepolia network id

--- src/utils/ChainId.ts
+++ src/utils/ChainId.ts
@@ -1,15 +1,18 @@
 class ChainId {
-    mainnet: u8 = 1
-    goerli: u8 = 5
+    mainnet: i32 = 1
+    goerli: i32 = 5
+    sepolia: i32 = 11155111
 }
 
 const chainId = new ChainId()
 
-export function getChainId(network: string): u8 {
+export function getChainId(network: string): i32 {
     if (network == "mainnet") {
         return chainId.mainnet
     } else if (network == "goerli") {
         return chainId.goerli
+    } else if (network == "sepolia") {
+        return chainId.sepolia
     }
     return chainId.mainnet
 }
