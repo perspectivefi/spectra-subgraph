03ad261b400adfc48391fd1926a99b685be28b34
ulydev <ulydev@live.fr>
2025-09-25 18:43:25 +0200

    chore: refactor epochId tracking

--- src/mappings/metavaults/metavaultWrapper.ts
+++ src/mappings/metavaults/metavaultWrapper.ts
@@ -7,7 +7,7 @@ import {
     RedeemRequest,
     DecreaseRedeemRequest,
     Deposit,
-    Withdraw
+    Withdraw,
 } from "../../../generated/Metavault/MetavaultWrapper"
 import { MetavaultWrapper as MetavaultWrapperAbi } from "../../../generated/Metavault/MetavaultWrapper"
 import { MetavaultWrapper } from "../../../generated/templates"
@@ -16,7 +16,6 @@ import { ZERO_BI } from "../../constants"
 import { updateAccountMetavaultRequest } from "../../entities/AccountAsset"
 import { getMetavault } from "../../entities/Metavault"
 import { AssetType } from "../../utils"
-
 
 export function handleMetaVaultWrapperInitialized(
     event: MetaVaultWrapperInitialized
@@ -35,58 +34,50 @@ export function handleMetaVaultWrapperInitialized(
 }
 
 export function handleDepositRequest(event: DepositRequest): void {
-    const epochId = getEpochId(event.address)
     updateAccountMetavaultRequest(
         event.params.owner,
         event.address,
         event.block.timestamp,
         AssetType.MV_REQUEST_DEPOSIT,
         "add",
-        event.params.assets,
-        epochId
+        event.params.assets
     )
 }
 
 export function handleDecreaseDepositRequest(
     event: DecreaseDepositRequest
 ): void {
-    const epochId = getEpochId(event.address)
     updateAccountMetavaultRequest(
         event.params.owner,
         event.address,
         event.block.timestamp,
         AssetType.MV_REQUEST_DEPOSIT,
         "set",
-        event.params.newRequestedAssets,
-        epochId
+        event.params.newRequestedAssets
     )
 }
 
 export function handleRedeemRequest(event: RedeemRequest): void {
-    const epochId = getEpochId(event.address)
     updateAccountMetavaultRequest(
         event.params.owner,
         event.address,
         event.block.timestamp,
         AssetType.MV_REQUEST_REDEEM,
         "add",
-        event.params.shares,
-        epochId
+        event.params.shares
     )
 }
 
 export function handleDecreaseRedeemRequest(
     event: DecreaseRedeemRequest
 ): void {
-    const epochId = getEpochId(event.address)
     updateAccountMetavaultRequest(
         event.params.owner,
         event.address,
         event.block.timestamp,
         AssetType.MV_REQUEST_REDEEM,
         "set",
-        event.params.newRequestedShares,
-        epochId
+        event.params.newRequestedShares
     )
     // what happens if:
     // 1. user requests deposit
@@ -95,7 +86,6 @@ export function handleDecreaseRedeemRequest(
 }
 
 export function handleDeposit(event: Deposit): void {
-    const epochId = getEpochId(event.address)
     // all deposit requests are cleared
     updateAccountMetavaultRequest(
         event.params.owner,
@@ -103,13 +93,11 @@ export function handleDeposit(event: Deposit): void {
         event.block.timestamp,
         AssetType.MV_REQUEST_DEPOSIT,
         "set",
-        ZERO_BI,
-        epochId
+        ZERO_BI
     )
 }
 
 export function handleWithdraw(event: Withdraw): void {
-    const epochId = getEpochId(event.address)
     // all redeem requests are cleared
     updateAccountMetavaultRequest(
         event.params.owner,
@@ -117,11 +105,6 @@ export function handleWithdraw(event: Withdraw): void {
         event.block.timestamp,
         AssetType.MV_REQUEST_REDEEM,
         "set",
-        ZERO_BI,
-        epochId
+        ZERO_BI
     )
-}
-
-function getEpochId(metavaultWrapperAddress: Address): BigInt {
-    return MetavaultWrapperAbi.bind(metavaultWrapperAddress).try_epochId().value
 }
