53a8343cbf9370b8d2602675c0217f9efb2579f4
Jean Chambras <jean@chambras.fr>
2025-11-19 11:04:33 +0100

    feat: add SafeModuleTransaction and SafeMultiSigTransaction events handling
    - Introduced new MetavaultTransaction entity to track transactions initiated by metavaults.
    - Updated schema.graphql to include MetavaultTransaction type.
    - Enhanced subgraph.template.yaml with new event handlers for SafeModuleTransaction and SafeMultiSigTransaction.
    - Implemented corresponding mapping functions in modules.ts to handle the new transaction events.

--- src/mappings/metavaults/modules.ts
+++ src/mappings/metavaults/modules.ts
@@ -1,20 +1,68 @@
-import { log } from "@graphprotocol/graph-ts"
+import { Address, log } from "@graphprotocol/graph-ts"
 
 import {
     TimelockedTransactionAdded,
     MetavaultModule,
+    MetavaultTransaction,
 } from "../../../generated/schema"
 import { GnosisSafeModule } from "../../../generated/templates"
-import { EnabledModule } from "../../../generated/templates/GnosisSAFE/GnosisSAFE"
+import { EnabledModule, SafeModuleTransaction, SafeMultiSigTransaction } from "../../../generated/templates/GnosisSAFE/GnosisSAFE"
 import { TransactionAdded } from "../../../generated/templates/GnosisSafeModule/GnosisSAFEModule"
+
+
+function getModule(moduleAddress: Address, metavaultAddress: Address): MetavaultModule | null {
+    const module = MetavaultModule.load(moduleAddress)
+    if (!module) {
+        const newModule = new MetavaultModule(moduleAddress)
+        newModule.address = moduleAddress
+        newModule.metavault = metavaultAddress.toHex()
+        newModule.save()
+        return newModule
+    }
+    return module
+}
 
 export function handleEnabledModule(event: EnabledModule): void {
     const moduleAddress = event.params.module
-    const metavaultModule = new MetavaultModule(moduleAddress)
-    metavaultModule.address = moduleAddress
-    metavaultModule.metavault = event.address.toHex()
-    metavaultModule.save()
+    const metavaultAddress = event.address
+    const module = getModule(moduleAddress, metavaultAddress)
+    if (!module) {
+        throw new Error("Module not found")
+    }
     GnosisSafeModule.create(moduleAddress)
+}
+
+export function handleSafeModuleTransaction(event: SafeModuleTransaction): void {
+    const transaction = new MetavaultTransaction(event.transaction.hash.concatI32(event.logIndex.toI32()))
+    const module = getModule(event.params.module, event.address)
+    if (!module) {
+        throw new Error("Module not found")
+    }
+    transaction.module = module.id
+    transaction.metavault = module.metavault
+    transaction.to = event.params.to
+    transaction.value = event.params.value
+    transaction.data = event.params.data
+    transaction.operation = event.params.operation
+    transaction.timestamp = event.block.timestamp
+    transaction.blockNumber = event.block.number
+    transaction.transactionHash = event.transaction.hash
+    transaction.logIndex = event.logIndex
+    transaction.save()
+}
+
+export function handleSafeMultiSigTransaction(event: SafeMultiSigTransaction): void {
+    const transaction = new MetavaultTransaction(event.transaction.hash.concatI32(event.logIndex.toI32()))
+    transaction.metavault = event.address.toHex()
+    transaction.to = event.params.to
+    transaction.value = event.params.value
+    transaction.data = event.params.data
+    transaction.operation = event.params.operation
+    transaction.timestamp = event.block.timestamp
+    transaction.blockNumber = event.block.number
+    transaction.transactionHash = event.transaction.hash
+    transaction.logIndex = event.logIndex
+    transaction.save()
 }
 
 export function handleTransactionAdded(event: TransactionAdded): void {
